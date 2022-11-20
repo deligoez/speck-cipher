f5996ab95b95ec8f2cac15aa5fb092a7f48d9886
Yunus Emre Deligöz <ye@deligoz.me>
2022-11-20 22:12:08 +0300

    EncryptBench: Separate encryption and decryption methods and introduce a setup method

--- tests/Benchmark/EncryptBench.php
+++ tests/Benchmark/EncryptBench.php
@@ -5,26 +5,46 @@ declare(strict_types=1);
 namespace Deligoez\Speck\Tests\Benchmark;
 
 use Deligoez\Speck\Speck;
+use GMP;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
+use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
 
 class EncryptBench
 {
-    /**
-     * @Revs(10000)
-     * @Iterations(5)
-     */
-    public function benchEncrypt(): void
+    protected Speck $cipher;
+    protected GMP $plaintxt;
+    protected GMP $ciphertxt;
+
+    public function setup(): void
     {
+        $this->plaintxt = gmp_init('0x6574694c');
+        $this->ciphertxt = gmp_init('0xa86842f2');
+
         $key = gmp_init('0x1918111009080100');
-        $plaintxt = gmp_init('0x6574694c');
-        $ciphertxt = gmp_init('0xa86842f2');
         $block_size = 32;
         $key_size = 64;
 
-        $cipher = new Speck($key, $key_size, $block_size);
+        $this->cipher = new Speck($key, $key_size, $block_size);
+    }
 
-        $cipher->encrypt($plaintxt);
-        $cipher->decrypt($ciphertxt);
+    /**
+     * @Revs(10000)
+     * @Iterations(5)
+     * @BeforeMethods({"setup"})
+     */
+    public function benchEncrypt(): void
+    {
+        $this->cipher->encrypt($this->plaintxt);
+    }
+
+    /**
+     * @Revs(10000)
+     * @Iterations(5)
+     * @BeforeMethods({"setup"})
+     */
+    public function benchDecrypt(): void
+    {
+        $this->cipher->decrypt($this->ciphertxt);
     }
 }
