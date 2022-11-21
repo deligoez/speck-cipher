c11b3a219b6b3fbe0c8e4d1b0df7ad5d25a919b7
Yunus Emre Deligöz <ye@deligoz.me>
2022-11-21 08:24:36 +0300

    Speck: Add method documentations

--- src/Speck.php
+++ src/Speck.php
@@ -8,6 +8,9 @@ use Deligoez\Speck\Exceptions\InvalidBlockSizeException;
 use Deligoez\Speck\Exceptions\InvalidKeySizeException;
 use GMP;
 
+/**
+ * Speck Block Cipher Object.
+ */
 class Speck
 {
     // blockSize => [[keySize => numberOfRounds]]
@@ -20,7 +23,18 @@ class Speck
     ];
     protected const VALID_MODES = ['ECB', 'CTR', 'CBC', 'PCBC', 'CFB', 'OFB'];
 
+    /** Integer representation of the encryption key */
+    protected int|GMP $key;
+
+    /** Integer representing the size of the encryption key in bits */
+    protected int $keySize;
+
+    /** Integer representing the size of the blocks in bits */
+    protected int $blockSize;
+
+    /** Integer representing the size of the words in bits */
     protected int $wordSize;
+
     protected int $rounds;
     protected GMP $modMask;
     protected GMP $modMaskSub;
@@ -35,23 +49,28 @@ class Speck
     protected array $keySchedule;
 
     /**
+     * Initialize an instance of the Speck Block Cipher.
+     *
      * @throws \Deligoez\Speck\Exceptions\InvalidBlockSizeException
      * @throws \Deligoez\Speck\Exceptions\InvalidKeySizeException
      */
     public function __construct(
-        protected int|GMP $key,
-        protected int $keySize = 128,
-        protected int $blockSize = 128,
+        int|GMP $key,
+        int $keySize = 128,
+        int $blockSize = 128,
     ) {
         // Block size validation
-        if (! array_key_exists($this->blockSize, self::VALID_SETUPS)) {
+        if (! array_key_exists($blockSize, self::VALID_SETUPS)) {
             InvalidBlockSizeException::build();
         }
 
         // Key size validation
-        if (! array_key_exists($this->keySize, self::VALID_SETUPS[$this->blockSize])) {
-            InvalidKeySizeException::build($this->blockSize);
+        if (! array_key_exists($keySize, self::VALID_SETUPS[$blockSize])) {
+            InvalidKeySizeException::build($blockSize);
         }
+
+        $this->keySize = $keySize;
+        $this->blockSize = $blockSize;
 
         $this->wordSize = $this->blockSize >> 1;
         $this->rounds = self::VALID_SETUPS[$this->blockSize][$keySize];
@@ -60,14 +79,14 @@ class Speck
         $this->modMaskSub = gmp_pow(num: 2, exponent: $this->wordSize);
 
         // Create properly sized bit mask for truncating addition and left shift outputs
-        $this->modMask = gmp_sub($this->modMaskSub, num2: 1);
+        $this->modMask = gmp_sub(num1: $this->modMaskSub, num2: 1);
 
         // Setup circular shift parameters
         $this->betaShift = $this->blockSize === 32 ? 2 : 3;
         $this->alphaShift = $this->blockSize === 32 ? 7 : 8;
 
         // Parse the given key and truncate it to the key length
-        $this->key = gmp_and($this->key, ((gmp_pow(2, $this->keySize)) - 1));
+        $this->key = gmp_and($key, ((gmp_pow(2, $this->keySize)) - 1));
 
         // Pre-compile key schedule
         $this->keySchedule = [gmp_and($this->key, $this->modMask)];
@@ -88,14 +107,17 @@ class Speck
     /**
      * Complete one round of Feistel operation.
      *
+     * @param \GMP $upperWord Upper bits of the current plain text.
+     * @param \GMP $lowerWord Lower bits of the current plain text.
+     * @param \GMP $key       Round key.
      *
-     * @return array{0: GMP, 1: GMP }
+     * @return array{0: GMP, 1: GMP } Upper and lower cipher text segments.
      */
-    protected function round(GMP $upperWord, GMP $lowerWord, GMP $k): array
+    protected function round(GMP $upperWord, GMP $lowerWord, GMP $key): array
     {
         $upperWord = gmp_and(gmp_add($this->gmp_shiftl($upperWord, ($this->wordSize - $this->alphaShift)), $this->gmp_shiftr($upperWord, $this->alphaShift)), $this->modMask);
         $upperWord = gmp_and(gmp_add($upperWord, $lowerWord), $this->modMask);
-        $upperWord = gmp_xor($k, $upperWord);
+        $upperWord = gmp_xor($key, $upperWord);
 
         $lowerWord = gmp_and(gmp_add($this->gmp_shiftr($lowerWord, ($this->wordSize - $this->betaShift)), ($this->gmp_shiftl($lowerWord, $this->betaShift))), $this->modMask);
         $lowerWord = gmp_xor($upperWord, $lowerWord);
@@ -104,22 +126,29 @@ class Speck
     }
 
     /**
-     * Returns the encrypted word pairs.
+     * Completes appropriate number of Speck Feistel functions to encrypt provided words.
+     * Round number is based off of number of elements in the key schedule.
      *
+     * @param \GMP $upperWord Upper bits of the current plain text input. Limited by the word size of currenly configured cipher.
+     * @param \GMP $lowerWord Lower bits of the current plain text input. Limited by the word size of currenly configured cipher.
      *
-     * @return array{ 0: GMP, 1: GMP }
+     * @return array{ 0: GMP, 1: GMP } Upper and lower bits of the cipher text words.
      */
     protected function encryptRaw(GMP $upperWord, GMP $lowerWord): array
     {
-        foreach ($this->keySchedule as $k) {
-            [$upperWord, $lowerWord] = $this->round($upperWord, $lowerWord, $k);
+        foreach ($this->keySchedule as $key) {
+            [$upperWord, $lowerWord] = $this->round($upperWord, $lowerWord, $key);
         }
 
         return [$upperWord, $lowerWord];
     }
 
     /**
-     * Returns the encrypted plain text.
+     * Process a new plain text into the cipher text based on current cipher setup.
+     *
+     * @param \GMP|string|int $plainText Integer value to encrypt.
+     *
+     * @return \GMP Encrypted integer value
      */
     public function encrypt(GMP|string|int $plainText): GMP
     {
@@ -138,14 +167,17 @@ class Speck
     /**
      * Complete one round of reverse Feistel operation.
      *
+     * @param \GMP $upperWord Upper bits of the current cipher text.
+     * @param \GMP $lowerWord Lower bits of the current cipher text.
+     * @param \GMP $key       Round key.
      *
-     * @return array{0: GMP, 1: GMP }
+     * @return array{0: GMP, 1: GMP } Upper and lower plain text segments.
      */
-    protected function reverseRound(GMP $upperWord, GMP $lowerWord, GMP $k): array
+    protected function reverseRound(GMP $upperWord, GMP $lowerWord, GMP $key): array
     {
         $upperWord = gmp_xor($upperWord, $lowerWord);
         $upperWord = gmp_and(gmp_add($this->gmp_shiftl($upperWord, ($this->wordSize - $this->betaShift)), ($this->gmp_shiftr($upperWord, $this->betaShift))), $this->modMask);
-        $upperWord = gmp_xor($upperWord, $k);
+        $upperWord = gmp_xor($upperWord, $key);
 
         $lowerWord = gmp_mod(gmp_add(gmp_sub($upperWord, $lowerWord), $this->modMaskSub), $this->modMaskSub);
         $lowerWord = gmp_and(gmp_add($this->gmp_shiftr($lowerWord, ($this->wordSize - $this->alphaShift)), ($this->gmp_shiftl($lowerWord, $this->alphaShift))), $this->modMask);
@@ -154,24 +186,31 @@ class Speck
     }
 
     /**
-     * Returns the decrypted word pairs.
+     * Completes appropriate number of Speck Feistel functions to decrypt provided words.
+     * Round number is based off of number of elements in the key schedule.
      *
+     * @param \GMP $upperWord Upper bits of the current cipher text input. Limited by the word size of currenly configured cipher.
+     * @param \GMP $lowerWord Lower bits of the current cipher text input. Limited by the word size of currenly configured cipher.
      *
-     * @return array{ 0: GMP, 1: GMP }
+     * @return array{ 0: GMP, 1: GMP } Upper and lower bits of the plain text words.
      */
     protected function decryptRaw(GMP $upperWord, GMP $lowerWord): array
     {
-        foreach (array_reverse($this->keySchedule) as $k) {
-            $this->reverseRound($upperWord, $lowerWord, $k);
+        foreach (array_reverse($this->keySchedule) as $key) {
+            $this->reverseRound($upperWord, $lowerWord, $key);
         }
 
         return [$upperWord, $lowerWord];
     }
 
     /**
-     * Returns the decrypted plain text.
+     * Process a cipher text into the plain text based on current cipher setup.
+     *
+     * @param \GMP|string|int $ciphertext Integer value to decrypt.
+     *
+     * @return \GMP Decrypted integer value
      */
-    public function decrypt(GMP|string|int $ciphertext): GMP|int
+    public function decrypt(GMP|string|int $ciphertext): GMP
     {
         $b = gmp_and($this->gmp_shiftr($ciphertext, $this->wordSize), $this->modMask);
         $a = gmp_and($ciphertext, $this->modMask);
@@ -185,11 +224,25 @@ class Speck
 
     // region Helpers
 
+    /**
+     * Bitwise left shift.
+     * Moves all bits in the number to the left by the given number of places.
+     *
+     * @param \GMP|string|int $number         Number to bitwise left shift.
+     * @param int             $numberOfShifts Number of places to shift.
+     */
     protected function gmp_shiftl(GMP|string|int $number, int $numberOfShifts): GMP
     {
         return gmp_mul($number, gmp_pow(2, $numberOfShifts));
     }
 
+    /**
+     * Bitwise right shift.
+     * Moves all bits in the number to the right by the given number of places.
+     *
+     * @param \GMP|string|int $number         Number to bitwise right shift.
+     * @param int             $numberOfShifts Number of places to shift.
+     */
     protected function gmp_shiftr(GMP|string|int $number, int $numberOfShifts): GMP
     {
         return gmp_div($number, gmp_pow(2, $numberOfShifts));
